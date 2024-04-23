8c0ca684d154828e762e5e1f2e3fd910da393c0d
realteck-ky <32591276+realteck-ky@users.noreply.github.com>
2024-04-24 03:07:28 +0900

    Extend timeout for CI build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  staticPageGenerationTimeout: 1000,
   reactStrictMode: true,
   images: {
     unoptimized: true
